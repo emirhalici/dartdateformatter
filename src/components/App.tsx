d10a07bd2be4963a22ec9eb6f72fb68f0e2e179f
Emir Halıcı <emir.halici1@gmail.com>
2023-08-21 00:51:56 +0300

    feat: update App for router support

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,28 @@
 import HomePage from 'pages/Home'
+import { PropsWithChildren, Suspense } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoadingOrError from './Avatar/LoadingOrError'
 
-function App() {
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<LoadingOrError />}>
+        <AppWrapper>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </AppWrapper>
+      </Suspense>
+    </BrowserRouter>
+  )
+}
+
+function AppWrapper(props: PropsWithChildren) {
   return (
     <div className="relative overflow-hidden bg-white dark:bg-neutral-950">
       <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-        <HomePage></HomePage>
+        {props.children}
       </div>
     </div>
   )
 }
-
-export default App
